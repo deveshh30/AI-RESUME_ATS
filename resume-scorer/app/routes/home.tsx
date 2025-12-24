143c6f22a0945d0657ba26fa8b63d6a6dc242103
deveshh30 <devesh30gupta@gmail.com>
2025-12-24 23:30:38 +0530

    donne with homepg

--- resume-scorer/app/routes/home.tsx
+++ resume-scorer/app/routes/home.tsx
@@ -14,15 +14,15 @@ export default function Home() {
   return <main>
     <section className="main-section bg-[url('resume-scorer/public/imgs/bg-main.svg')] bg-cover">
       <Navbar/>
-      <div className="page-head">
-        <h1>Track Your APPLICATIONS & Resume Rating</h1>
-        <h2>review your submission and check AI-powered feedback.
+      <div className="page-head py-13 flex flex-col text-center">
+        <h1 className="uppercase">Track Your APPLICATIONS & Resume Rating</h1>
+        <h2>Review your submission and check AI-powered feedback.
         </h2>
       </div>
     </section>
 
     {resume.length > 0 && (
-      <div className="resume-section">
+      <div className="resumes-section">
         {resume.map((item) => (
           <ResumeCard key={item.id} resume={item} />
         ))}
