db4546cd3a4e7633720c5c8a6289ca98027f9a97
deveshh30 <devesh30gupta@gmail.com>
2025-12-27 16:43:37 +0530

    creating a signup page for authorization

---
+++ resume-scorer/app/routes/auth.tsx
@@ -0,0 +1,20 @@
+export const meta = () => ([
+    {title : 'Hire-Scan | AUTH' },
+    {name : 'description', content : 'Log into your account'},
+])
+
+import React from 'react'
+
+function auth() {
+  return (
+    <main className='bg-[url("resume-scorer/public/images/bg-auth.svg")] bg-cover min-h-screen flex items-center justify-center'>
+        <div className="gradient-border- shadow-lg ">
+          <section className='flex flex-col gap-8 bg-white rounded-xl p-10'>
+              
+          </section>
+        </div>
+    </main>
+  )
+}
+
+export default auth
